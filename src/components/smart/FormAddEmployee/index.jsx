0d3909c77573a5ff1fa75d38923d479af7c16e24
Boubekeur <rachidboud>
2021-08-14 03:59:34 +0200

    :hammer: development script for Modal component dumb

--- src/components/smart/FormAddEmployee/index.jsx
+++ src/components/smart/FormAddEmployee/index.jsx
@@ -1,54 +1,64 @@
+import { useState } from 'react';
+import { Modal } from '../../dumb/Modal';
 import './formAddEmployee.css';
 
 /**
  * FormAddEmployee component smart
  */
 export const FormAddEmployee = () => {
+    const [modal, setModal] = useState(false);
+    const HandleForm = (e) => {
+        e.preventDefault();
+        setModal(true);
+    };
     return (
-        <form className="FormAddEmployee">
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" />
+        <div>
+            <form className="FormAddEmployee" onSubmit={HandleForm}>
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" id="first-name" />
 
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" />
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" id="last-name" />
 
-            <label htmlFor="date-of-birth">Date of Birth</label>
-            <input id="date-of-birth" type="date" />
+                <label htmlFor="date-of-birth">Date of Birth</label>
+                <input id="date-of-birth" type="date" />
 
-            <label htmlFor="start-date">Start Date</label>
-            <input id="start-date" type="date" />
+                <label htmlFor="start-date">Start Date</label>
+                <input id="start-date" type="date" />
 
-            <fieldset>
-                <legend>Address</legend>
+                <fieldset>
+                    <legend>Address</legend>
 
-                <label htmlFor="street">Street</label>
-                <input id="street" type="text" />
+                    <label htmlFor="street">Street</label>
+                    <input id="street" type="text" />
 
-                <label htmlFor="city">City</label>
-                <input id="city" type="text" />
+                    <label htmlFor="city">City</label>
+                    <input id="city" type="text" />
 
-                <label htmlFor="state">State</label>
-                <select name="state" id="state">
-                    <option>France</option>
-                    <option>France</option>
-                    <option>France</option>
-                    <option>France</option>
-                    <option>France</option>
+                    <label htmlFor="state">State</label>
+                    <select name="state" id="state">
+                        <option>France</option>
+                        <option>France</option>
+                        <option>France</option>
+                        <option>France</option>
+                        <option>France</option>
+                    </select>
+
+                    <label htmlFor="zip-code">Zip Code</label>
+                    <input id="zip-code" type="number" />
+                </fieldset>
+
+                <label htmlFor="department">Department</label>
+                <select name="department" id="department">
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Engineering</option>
+                    <option>Human Resources</option>
+                    <option>Legal</option>
                 </select>
-
-                <label htmlFor="zip-code">Zip Code</label>
-                <input id="zip-code" type="number" />
-            </fieldset>
-
-            <label htmlFor="department">Department</label>
-            <select name="department" id="department">
-                <option>Sales</option>
-                <option>Marketing</option>
-                <option>Engineering</option>
-                <option>Human Resources</option>
-                <option>Legal</option>
-            </select>
-            <input type="submit" className="btnSave" value="Save" />
-        </form>
+                <input type="submit" className="btnSave" value="Save" />
+            </form>
+            {modal && <Modal setModal={setModal} />}
+        </div>
     );
 };
