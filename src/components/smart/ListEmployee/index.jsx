2b1a67ea93325eb06f36e5af1fbf4dd999ca1ba8
Rachid Boubekeur <rachidboubekeur@free.fr>
2021-08-18 04:32:19 +0200

    :hammer: begin development script for ListEmployee component smart

--- src/components/smart/ListEmployee/index.jsx
+++ src/components/smart/ListEmployee/index.jsx
@@ -4,5 +4,54 @@ import './listEmployee.css';
  * ListEmployee component smart
  */
 export const ListEmployee = () => {
-    return <p>ListEmployee</p>;
+    return (
+        <div className="listEmployee">
+            <div className="showSearch">
+                <div>
+                    <label htmlFor="show">
+                        Show
+                        <select id="show">
+                            <option>10</option>
+                            <option>25</option>
+                            <option>50</option>
+                            <option>100</option>
+                        </select>
+                        entries
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="search">Search: </label>
+                    <input type="search" id="search" />
+                </div>
+            </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>First Name <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Last Name <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Start Date <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Department <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Date of Birth <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Street <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>City <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>State <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                        <th>Zip Code <p><i className="fas fa-caret-up"></i><i className="fas fa-caret-down"></i></p></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th colSpan="9">No data available in table</th>
+                    </tr>
+                </tbody>
+            </table>
+            <div className="showingPreviousOrNext">
+                <div>
+                    <p>Showing 0 to 0 of 0 entries</p>
+                </div>
+                <div>
+                    <p><span>Previous</span><span>Next</span></p>
+                </div>
+            </div>
+        </div>
+    );
 };
