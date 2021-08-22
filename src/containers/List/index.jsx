61271c06ccc58df7afd44ca66b55557b96cbf58a
Rachid Boubekeur <rachidboubekeur@free.fr>
2021-08-22 16:34:51 +0200

    :hammer: add hrnet-table in List container

--- src/containers/List/index.jsx
+++ src/containers/List/index.jsx
@@ -1,15 +1,18 @@
-import { ListEmployee } from '../../components/smart/ListEmployee';
+import { useSelector } from 'react-redux';
+import { employeeSelector } from '../../slice/Employee';
+import Table from 'hrnet-table';
 import './list.css';
 
 /**
  * List container
  */
 export const List = () => {
+    const state = useSelector(employeeSelector);
     return (
         <section className="List">
             <div>
                 <h1>List Employee</h1>
-                <ListEmployee />
+                <Table state={state} />
             </div>
         </section>
     );
